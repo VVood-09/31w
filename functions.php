4bb7eeb18143c906b728b8361e6e8c42e8508ee5
VVood-09 <e2295393@cmaisonneuve.qc.ca>
2022-11-10 11:16:08 -0500

    C17 - Ajout du filtre d'événement dans functions.php. Test css.

--- functions.php
+++ functions.php
@@ -166,3 +166,21 @@ function my_register_sidebars() {
 	);
 	/* Repeat register_sidebar() code for additional sidebars. */
 }
+
+/* ----------------------------------------------------------- Ajout de la description dans menu */
+/**
+ * Filtre du menu evenement
+ * @param string $item_output	chaîne représentant l'élément du menu
+ * @param object $item 			élément du menu
+ */
+function prefix_nav_description( $item_output, $item) {
+    if ( !empty( $item->description ) ) {
+        $item_output = str_replace( '</a>',
+        '<hr><span class="menu-item-description">' . $item->description . '</span><div class="menu-item-icone"></div></a>',
+              $item_output );
+    }
+    return $item_output;
+}
+add_filter( 'walker_nav_menu_start_el', 'prefix_nav_description', 10, 2 );
+// l'argument 10 : niveau de privilège
+// l'argument 2 : le nombre d'argument dans la fonction de rappel: «prefix_nav_description»
