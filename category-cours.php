3ae74dc3f8c50c88c97b6e499b3c4f1284999fea
VVood-09 <e2295393@cmaisonneuve.qc.ca>
2022-11-11 14:42:14 -0500

    C17.5 - Gestion du flex dans la page category-cours.

--- category-cours.php
+++ category-cours.php
@@ -19,15 +19,15 @@
 <!-- <h1 class="trace">front-page.php</h1> -->
 
 <body>
-    <main class="site__main">
+    <main class="site__main category__cours">
         <?php
         if (have_posts()) :
             while (have_posts()) :
                 the_post(); ?>
                 <article class="main__post">
-                    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    <h2><a href="<?php the_permalink(); ?>"><?= substr(explode("(", get_the_title())[0], 8); ?></a></h2>
                     <h3>Durée du cours: <?php the_field('duree'); ?></h3>
-                    <?php the_content(null, true); ?>
+                    <?= "<p>".wp_trim_words(get_the_excerpt(), 30, "...")."</p>"; ?>
                 </article>
         <?php endwhile;
         endif;
