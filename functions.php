7146cc95235425149a863d169ed2845dfd48d4ab
VVood-09 <e2295393@cmaisonneuve.qc.ca>
2022-11-24 09:27:26 -0500

    EF - Création des sidebars dans functions.php.

--- functions.php
+++ functions.php
@@ -176,6 +176,28 @@ function my_register_sidebars() {
 			'after_title'   => '</h3>',
 		)
 	);
+	register_sidebar(
+		array(
+			'id'            => 'header-recherche',
+			'name'          => __( 'Recherche' ),
+			'description'   => __( 'Outil de recherche' ),
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</div>',
+			'before_title'  => '<h3 class="widget-title">',
+			'after_title'   => '</h3>',
+		)
+	);
+	register_sidebar(
+		array(
+			'id'            => 'header-sociaux',
+			'name'          => __( 'Réseaux sociaux' ),
+			'description'   => __( 'Icône avec liens pour les réseaux sociaux' ),
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</div>',
+			'before_title'  => '<h3 class="widget-title">',
+			'after_title'   => '</h3>',
+		)
+	);
 	/* Repeat register_sidebar() code for additional sidebars. */
 }
 
